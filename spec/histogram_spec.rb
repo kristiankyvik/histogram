9e3385c040a9bd5c1c30e5edb4e383c191fc4ccd
Kristian Ruiz Kyvik <kristianruizkyvik@gmail.com>
2014-07-14 16:52:46 +0200

    added many functionality and tests

--- spec/histogram_spec.rb
+++ spec/histogram_spec.rb
@@ -8,13 +8,20 @@ class Histogram
 
 
 	def for(pois)
-		
+
 		if pois == []
 			return {}
 		end
-		poi=pois[0]
-		hist=Hash.new #={rating=>cuantos}
-		hist[poi.rating]=1
+
+		hist=Hash.new 
+		for point in pois
+			if (hist[point.rating] == nil)
+				hist[point.rating] = 1
+			
+			else
+				hist[point.rating] = (hist[point.rating] + 1)
+			end
+		end
 		hist
 	end
 end
@@ -31,4 +38,19 @@ describe "Histograms" do
 		p1=POI.new("sagrada", "muntaner", 5)
 		expect(Histogram.new.for([p1])).to eq({5=>1})
 	end
+
+	it "exactly two" do
+		p1=POI.new("sagrada", "muntaner", 5)
+		p2=POI.new("torre", "catalonia", 4)
+		expect(Histogram.new.for([p1,p2])).to eq({4=>1, 5=>1})
+	end
+
+	it "if more than two" do
+		p1=POI.new("sagrada", "muntaner", 5)
+		p2=POI.new("torre", "catalonia", 4)
+		p3=POI.new("jamon", "serrano", 5)
+		p4=POI.new("torre jimenz", "bilbao", 1)
+		expect(Histogram.new.for([p1,p2,p3,p4])).to eq({1=>1, 4=>1, 5=>2})
+	end
+
 end
